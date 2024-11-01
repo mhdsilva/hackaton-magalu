5e48ac460ba64b226d680d97f34b05d624e9519f
Matheus Silva <matheus.silva@beuni.com.br>
2024-11-01 02:22:40 -0300

    change resource

--- transcriptMGC/blocks/resource.py
+++ transcriptMGC/blocks/resource.py
@@ -1,94 +1,109 @@
 class ResourceTransformer:
     def __init__(self, aws_resource):
+
         self.aws_resource = aws_resource
         self.unmapped_attributes = []
 
     def transform(self):
-        transformed_resources = []
         resource_type = list(self.aws_resource.keys())[0]
         resource_configs = self.aws_resource[resource_type]
+
+        if resource_type == 'aws_instance':
+            return self.transform_ec2_instance(resource_type, resource_configs)
+        elif resource_type == 'aws_s3_bucket':
+            return self.transform_s3_bucket(resource_type, resource_configs)
+        else:
+            print(f"Tipo de recurso '{resource_type}' não é suportado para transformação.")
+            return None
+
+    def transform_ec2_instance(self, resource_type, resource_configs):
+
+        transformed_resource = {}
         for resource_name, attributes in resource_configs.items():
-            if resource_type == 'aws_instance':
-                transformed_resource = self.transform_ec2_instance(resource_name, attributes)
-            elif resource_type == 'aws_s3_bucket':
-                transformed_resource = self.transform_s3_bucket(resource_name, attributes)
+            mgc_resource = {
+                'mgc_virtual_machine_instances': {
+                    resource_name: {
+                        'name': attributes.get('tags', {}).get('Name', resource_name),
+                        'machine_type': {
+                            'name': self.map_instance_type(attributes.get('instance_type'))
+                        },
+                        'image': {
+                            'name': self.map_ami(attributes.get('ami'))
+                        },
+                        'ssh_key_name': attributes.get('key_name', '')
+                    }
+                }
+            }
+
+            # Configuração de rede
+            network = {}
+            associate_public_ip = attributes.get('associate_public_ip_address')
+            network['associate_public_ip'] = associate_public_ip if associate_public_ip is not None else False
+
+            # Grupos de Segurança
+            security_groups = attributes.get('vpc_security_group_ids', [])
+            if security_groups:
+                network['interface'] = {
+                    'security_groups': [{'id': sg_id} for sg_id in security_groups]
+                }
+
+            if network:
+                mgc_resource['mgc_virtual_machine_instances'][resource_name]['network'] = network
+
+            # Informar sobre atributos não mapeados
+            known_attributes = {
+                'ami', 'instance_type', 'key_name',
+                'associate_public_ip_address', 'vpc_security_group_ids', 'tags'
+            }
+            extra_attributes = set(attributes.keys()) - known_attributes
+            if extra_attributes:
+                print(f"Atributos não mapeados em aws_instance '{resource_name}': {extra_attributes}")
+                self.unmapped_attributes.extend(extra_attributes)
+
+            transformed_resource.update(mgc_resource)
+
+        return transformed_resource
+
+    def transform_s3_bucket(self, resource_type, resource_configs):
+
+        transformed_resource = {}
+        for bucket_name, attributes in resource_configs.items():
+            mgc_resource = {
+                'mgc_object_storage_buckets': {
+                    bucket_name: {
+                        'bucket': attributes.get('bucket', bucket_name),
+                        'enable_versioning': attributes.get('versioning', {}).get('enabled', False)
+                    }
+                }
+            }
+
+            # Controle de Acesso
+            acl = attributes.get('acl', 'private')
+            acl_mapping = {
+                'private': {'private': True},
+                'public-read': {'public_read': True},
+                'public-read-write': {'public_read_write': True},
+                'authenticated-read': {'authenticated_read': True}
+                # Outras ACLs podem ser adicionadas se suportadas
+            }
+            mgc_acl = acl_mapping.get(acl)
+            if mgc_acl:
+                mgc_resource['mgc_object_storage_buckets'][bucket_name].update(mgc_acl)
             else:
-                print(f"Tipo de recurso '{resource_type}' não é suportado para transformação.")
-                continue
-            transformed_resources.append(transformed_resource)
-        return transformed_resources
+                print(f"ACL '{acl}' em aws_s3_bucket '{bucket_name}' não é suportada e será definida como 'private' por padrão.")
+                self.unmapped_attributes.append('acl')
+                mgc_resource['mgc_object_storage_buckets'][bucket_name]['private'] = True
 
-    def transform_ec2_instance(self, resource_name, attributes):
-        mgc_resource = {
-            'mgc_virtual_machine_instances': {
-                resource_name: {
-                    'name': attributes.get('tags', {}).get('Name', resource_name),
-                    'machine_type': {
-                        'name': self.map_instance_type(attributes.get('instance_type'))
-                    },
-                    'image': {
-                        'name': self.map_ami(attributes.get('ami'))
-                    },
-                    'ssh_key_name': attributes.get('key_name', '')
-                }
-            }
-        }
+            # Informar sobre atributos não mapeados
+            known_attributes = {'bucket', 'acl', 'versioning', 'tags'}
+            extra_attributes = set(attributes.keys()) - known_attributes
+            if extra_attributes:
+                print(f"Atributos não mapeados em aws_s3_bucket '{bucket_name}': {extra_attributes}")
+                self.unmapped_attributes.extend(extra_attributes)
 
-        network = {}
-        if attributes.get('associate_public_ip_address') is not None:
-            network['associate_public_ip'] = attributes.get('associate_public_ip_address')
-        else:
-            network['associate_public_ip'] = False
+            transformed_resource.update(mgc_resource)
 
-        security_groups = attributes.get('vpc_security_group_ids', attributes.get('security_groups', []))
-        if security_groups:
-            network['interface'] = {
-                'security_groups': [{'id': sg_id} for sg_id in security_groups]
-            }
-
-        if network:
-            mgc_resource['mgc_virtual_machine_instances'][resource_name]['network'] = network
-
-        known_attributes = {'ami', 'instance_type', 'key_name', 'associate_public_ip_address', 'vpc_security_group_ids', 'security_groups', 'tags'}
-        extra_attributes = set(attributes.keys()) - known_attributes
-        if extra_attributes:
-            print(f"Atributos não mapeados em aws_instance '{resource_name}': {extra_attributes}")
-            self.unmapped_attributes.extend(extra_attributes)
-
-        return mgc_resource
-
-    def transform_s3_bucket(self, resource_name, attributes):
-        mgc_resource = {
-            'mgc_object_storage_buckets': {
-                resource_name: {
-                    'bucket': attributes.get('bucket', resource_name),
-                    'enable_versioning': attributes.get('versioning', {}).get('enabled', False)
-                }
-            }
-        }
-
-        # Controle de Acesso
-        acl = attributes.get('acl', 'private')
-        acl_mapping = {
-            'private': {'private': True},
-            'public-read': {'public_read': True},
-            'public-read-write': {'public_read_write': True},
-            'authenticated-read': {'authenticated_read': True}
-        }
-        mgc_acl = acl_mapping.get(acl)
-        if mgc_acl:
-            mgc_resource['mgc_object_storage_buckets'][resource_name].update(mgc_acl)
-        else:
-            print(f"ACL '{acl}' em aws_s3_bucket '{resource_name}' não é suportada e será definida como 'private' por padrão.")
-            self.unmapped_attributes.append('acl')
-
-        known_attributes = {'bucket', 'acl', 'versioning', 'tags'}
-        extra_attributes = set(attributes.keys()) - known_attributes
-        if extra_attributes:
-            print(f"Atributos não mapeados em aws_s3_bucket '{resource_name}': {extra_attributes}")
-            self.unmapped_attributes.extend(extra_attributes)
-
-        return mgc_resource
+        return transformed_resource
 
     def map_instance_type(self, aws_instance_type):
         instance_type_mapping = {
